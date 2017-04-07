412684481ab8ef42ff40d6cf48600491e565301b
Régis Lutter <rlutter@lemieuxbedard.com>
2017-04-07 11:52:10 -0400

    Remove Trim

--- app/components/Main.js
+++ app/components/Main.js
@@ -110,13 +110,13 @@ var Main = React.createClass({
         var listGamblers = [];
         for (var i = 0; i < textLines.length; i++) {
             var tmpText = textLines[i];
-            if(parseInt(tmpText.charAt(0)) != 'NaN'){
-                if(tmpText.indexOf('.') >= 0){
-                    tmpText = tmpText.substr(tmpText.indexOf('.')+1);
-                } else if (tmpText.indexOf(' ') > 0) {
-                    tmpText = tmpText.substr(tmpText.indexOf(' ')+1);
-                }
-            }
+            //if(parseInt(tmpText.charAt(0)) != 'NaN'){
+            //    if(tmpText.indexOf('.') >= 0){
+            //        tmpText = tmpText.substr(tmpText.indexOf('.')+1);
+            //    } else if (tmpText.indexOf(' ') > 0) {
+            //        tmpText = tmpText.substr(tmpText.indexOf(' ')+1);
+            //    }
+            //}
             listGamblers.push(tmpText.trim());
         }
         this.setState({
